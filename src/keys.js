e02b01492a7c2fd7967987a77108b7ae5c8eaf69
chiuleung <chiu-leung@Hotmail.com>
2019-04-27 18:32:46 -0400

    initial set up for mongodb

---
+++ src/keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+  database: {
+    URI: "mongodb://gliang14:Lgz6868@ds149146.mlab.com:49146/image-share"
+  }
+
+  /* for future api configs for example
+  
+  facebook: {
+    secret: "xyz"
+  }
+
+  google: {
+    secret: "xyz"
+  }
+
+  */
+};
---
+++ src/server/config.js
@@ -0,0 +1,5 @@
+module.exports = app => {
+  // settings 
+  app.set('port', process.env.PORT || 3000);
+  return app;
+}
