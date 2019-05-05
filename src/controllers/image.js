d07264e640733bb58d295581f7b2640b1dcb142e
kc-leung <chiu-leung@Hotmail.com>
2019-05-04 23:53:17 -0400

    routes and controllers

---
+++ src/controllers/image.js
@@ -0,0 +1,11 @@
+const ctrl = {};
+
+ctrl.index = (req, res) => {};
+
+ctrl.create = (req, res) => {};
+
+ctrl.like = (req, res) => {};
+
+ctrl.comment = (req, res) => {};
+
+module.exports = ctrl;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const home = require("../controllers/home");
+const image = require("../controllers/image");
+
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.send("Index page");
-  });
+  router.get("/", home.index);
+  router.get("/images/:image_id", image.index);
+  router.post("/images", image.create);
+  router.post("/images/:image_id/like", image.like);
+  router.get("/images/:image_id/comment", image.comment);
+
+  app.use(router);
 };
